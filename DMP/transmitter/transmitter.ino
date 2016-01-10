7088a444b8143d960f986d5df4989a16ddba00c3
Cristian Militaru <cristipiticul@yahoo.com>
2016-01-10 22:55:28 +0200

    DMP transmitter

---
+++ DMP/transmitter/transmitter.ino
@@ -0,0 +1,52 @@
+#define rfTransmitPin 15  //RF Transmitter pin = digital pin 4
+#define ledPin 13        //Onboard LED = digital pin 13
+
+long writeDelay = 10000;
+long startTime;
+void setup() {
+  pinMode(rfTransmitPin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  startTime = micros();
+  Serial.begin(9600);
+}
+
+void writeData(int data) {
+  Serial.print("Sending data: ");
+  Serial.println(data, DEC);
+  
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(rfTransmitPin, HIGH);
+    digitalWrite(ledPin, HIGH);
+    delayMicroseconds(writeDelay);
+  }
+  for (int i = 0; i < data; i++) {
+    digitalWrite(rfTransmitPin, LOW);
+    digitalWrite(ledPin, LOW);
+    delayMicroseconds(writeDelay);
+
+    digitalWrite(rfTransmitPin, HIGH);
+    digitalWrite(ledPin, HIGH);
+    delayMicroseconds(writeDelay);
+  }
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(rfTransmitPin, LOW);
+    digitalWrite(ledPin, LOW);
+    delayMicroseconds(writeDelay);
+  }
+}
+
+void loop() {
+  digitalWrite(rfTransmitPin, HIGH);
+  digitalWrite(ledPin, HIGH);
+  delayMicroseconds(writeDelay);
+
+  digitalWrite(rfTransmitPin, LOW);
+  digitalWrite(ledPin, LOW);
+  delayMicroseconds(writeDelay);
+
+  int readData = (analogRead(16) - 150) / 97;
+  Serial.print("I received: ");
+  Serial.println(readData, DEC);
+
+  writeData(readData);
+}
